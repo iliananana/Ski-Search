f9f99621d359b606f2760725798ed254a73effad
iliana <cpiliana@gmail.com>
2023-04-12 17:37:56 -0600

    added web map

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+app=Flask(__name__)
+
+@app.route('/')
+def root():
+    markers=[
+        {
+        'lat':40.573710,
+        'lon':-105.081020,
+        'popup':'Starting at the CSU CS Building'
+        }
+    ]
+    return render_template('index.html',markers=markers )
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=8080, debug=True)
